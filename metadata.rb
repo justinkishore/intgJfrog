93fdb143fb549e73cf26609d1a536dd7f977586a
kishore <justinkishore@gmail.com>
2016-11-04 00:49:55 -0500

    Second commit

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name             'intgJfrog'
+name             'ChefAutomate'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures intgJfrog'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
 
 #depends "installArtifact"
 #depends "artifact"
